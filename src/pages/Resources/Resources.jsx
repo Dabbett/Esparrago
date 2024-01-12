6ec269f4326271db339885bcb0f92cca13b56a50
Dabbett <abbettdylan@gmail.com>
2024-01-12 11:40:50 -0500

    change case

---
+++ src/pages/Resources/Resources.jsx
@@ -0,0 +1,13 @@
+import React, {useState} from "react";
+import styles from "./Resources.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Resources = () => {
+    return (
+        <>
+        <Navbar />
+        <h1>Resources</h1>
+        </>
+    )
+    
+}
