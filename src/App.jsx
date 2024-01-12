3167c7832a45fc8e5beb1c47ab814477ddfae30c
Dabbett <abbettdylan@gmail.com>
2024-01-12 12:02:47 -0500

    fix case

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Hero } from "./components/Hero/Hero";
 import styles from "./App.module.css";
 import { Navbar } from "./components/Navbar/Navbar";
 import { About } from "./components/About/About";
-import { Specialties } from "./components/specialties/specialties";
+import { Specialties } from "./components/Specialties/Specialties";
 import { Adult } from "./components/Adult/Adult";
 import { Pediatric } from "./components/Pediatric/Pediatric";
 import { Footer } from "./components/Footer/Footer"
