6d21f482a36bf62fd53b43eb30eba411d93981ac
Dabbett <abbettdylan@gmail.com>
2024-01-23 13:45:47 -0500

    add contact page functionailty

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,18 +1,35 @@
 import React from "react";
 import styles from "./Contact.module.css";
 import { getImageUrl } from "../../utils";
+import emailjs from "@emailjs/browser";
+
 
 export const Contact = () => {
+
+  const sendEmail = (e) => {
+    
+    alert("Youre E-mail had been submitted! :)");
+
+    emailjs.sendForm(
+      "service_1jqnnfi",
+      "template_e4gthbu",
+      // "from_name",
+      // "from_email",
+      e.target,
+      "RagmeypRFbpOVDT4i"
+    );
+  }
+
   return (
     <section className={styles.contact}>
       <hr />
       <div className={styles.content}>
         <div className={styles.form}>
           <header>Contact Me</header>
-          <form id="form" className={styles.topBefore}>
-            <input id="name" type="text" placeholder="NAME" />
-            <input id="email" type="email" placeholder="E-MAIL" />
-            <textarea id="message" placeholder="MESSAGE"></textarea>
+          <form id="form" className={styles.topBefore} onSubmit={sendEmail}>
+            <input id="name" type="text" placeholder="NAME" name="From_name"/>
+            <input id="email" type="email" placeholder="E-MAIL" name="email_from"/>
+            <textarea id="message" placeholder="MESSAGE" name="message"></textarea>
             <button type="submit">GO!</button>
           </form>
         </div>
