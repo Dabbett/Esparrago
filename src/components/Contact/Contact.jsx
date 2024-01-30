09c34fd09a70ce9a4b69c648998ca8a47d21e5ba
Dabbett <abbettdylan@gmail.com>
2024-01-30 12:18:51 -0500

    alter contact form

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -8,13 +8,14 @@ export const Contact = () => {
 
   const sendEmail = (e) => {
     
-    alert("Youre E-mail had been submitted! :)");
+    alert("Youre E-mail had been submitted!");
 
     emailjs.sendForm(
       "service_1jqnnfi",
       "template_e4gthbu",
       "from_name",
       "from_email",
+      "from_location",
       e.target,
       "RagmeypRFbpOVDT4i"
     );
@@ -28,9 +29,10 @@ export const Contact = () => {
           <header>Contact Me</header>
           <form id="form" className={styles.topBefore} onSubmit={sendEmail}>
             <input id="name" type="text" placeholder="NAME" name="from_name"/>
-            <input id="email" type="email" placeholder="E-MAIL" name="from_email"/>
+            <input id="email" type="email" placeholder="E-MAIL" name="from_email" />
+            <input id="location" type="location" placeholder="YOUR LOCATION" name="from_location"/>
             <textarea id="message" placeholder="MESSAGE" name="message"></textarea>
-            <button classname={styles.submit} type="submit">Submit</button>
+            <button className={styles.submit} id="submit" type="submit">SUBMIT</button>
           </form>
         </div>
       </div>
